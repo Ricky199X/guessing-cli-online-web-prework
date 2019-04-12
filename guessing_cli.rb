b1e4e2b1a175baa9a6889ddcac75d676b65d26bb
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-12 14:07:27 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,10 @@
 def run_guessing_game
   input == nil
-  
+  computer_guess = rand(1...6)
+
   puts "Guess a number between 1 and 6."
   
-  until input == 
+  until input == computer_guess
+  
+  
 end
